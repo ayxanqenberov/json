67daa9bd43dcacba1bc168da6d80554962f615d5
098100ayxan <ganbarov008@gmail.com>
2024-08-13 11:36:26 +0400

    done

---
+++ main.js
@@ -0,0 +1,35 @@
+import newArray from "./module.js";
+// console.log(newArray);
+// function getFunc(array,name) {
+//     const getData = array.filter(item => item.address.street || item.address.city)
+//     try {
+//         if((getData) && name == array.name){
+//             console.log(`${getData.street} + ${getData.city}`);
+            
+//         }else{
+//             throw new Error("yoxdur")
+//         }
+//     }catch(err){
+//         console.log(err.message);
+        
+//     }
+// }
+// getFunc(newArray,"Kurtis Weissnat")
+
+function getFunt(array, id) {
+        const foundObject = array.find(item => item.id == id);
+    try{
+        if(foundObject){
+            console.log(`${foundObject}`);
+            
+        }else{
+            throw new Error(`bele id yoxdur`)
+        }
+    }catch(error){
+        console.log(error.message);
+        
+    }
+}
+
+getFunt(newArray,10)
+export default {getFunt}
